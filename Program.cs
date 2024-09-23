7144c4294aca83727c23c21f73d1d3257a37ab27
MustafaUcler <mustafa.plastal@gmail.com>
2024-09-23 21:07:30 +0200

    Färsta commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+Console.Write("Skriv in en siffra: ");
+
+int sum = int.Parse(Console.ReadLine());
+
+for (int i = 0; i <= sum; i++)
+{
+        if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+    
+}
